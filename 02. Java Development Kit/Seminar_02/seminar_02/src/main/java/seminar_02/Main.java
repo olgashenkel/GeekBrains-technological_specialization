b21b40662c5866d0d4380e0634956185b2cf7e47
olgashenkel <olga_shenkel@mail.ru>
2025-09-22 23:29:51 +0200

    update 02. Java Development Kit

--- 02. Java Development Kit/Seminar_02/seminar_02/src/main/java/seminar_02/Main.java
+++ 02. Java Development Kit/Seminar_02/seminar_02/src/main/java/seminar_02/Main.java
@@ -4,7 +4,7 @@ package seminar_02;
 public class Main {
     public static void main(String[] args) {
         Pair<String, String> stringPair = new Pair<>("Hello,", "World!");
-        System.out.println(PairUtils.concatenate(stringPair)); // Вывод: Hello World
+        System.out.println(PairUtils.concatenate(stringPair)); // Вывод: Hello, World!
 
         Pair<Integer, Integer> intPair = new Pair<>(5, 10);
         System.out.println(PairUtils.concatenate(intPair)); // Вывод: (5, 10)
